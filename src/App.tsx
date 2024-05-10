98473140cd82b995f0cf6697ee77b02b199854f6
SrikanthBethi9885 <srikanth.bethi@missiong3.com>
2024-05-10 16:31:38 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,26 +12,26 @@ import LogiPage from "./MCPC-APP/Pages/LogiPage";
 const App = () => {
 
   return (
-    // <Router>
-    //   <div className="custom-layout">
-    //     <Header />
-    //     <div className="main-content">
-    //       <Sidebar />
-    //       <div className="content">
-    //         <Routes>
-    //           <Route path="/purchases" element={<Purchases />} />
-    //           <Route path="/inventory" element={<Inventory />} />
-    //           <Route path="/tickets" element={<Tickets />} />
-    //           <Route path="/orders" element={<Orders />} />
-    //           <Route path="/deployment" element={<Deployment />} />
-    //           <Route path="/assets" element={<Assets />} />
-    //         </Routes>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </Router>
+    <Router>
+      <div className="custom-layout">
+        <Header />
+        <div className="main-content">
+          <Sidebar />
+          <div className="content">
+            <Routes>
+              <Route path="/purchases" element={<Purchases />} />
+              <Route path="/inventory" element={<Inventory />} />
+              <Route path="/tickets" element={<Tickets />} />
+              <Route path="/orders" element={<Orders />} />
+              <Route path="/deployment" element={<Deployment />} />
+              <Route path="/assets" element={<Assets />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
     // <Layout />
-    <LogiPage />
+    // <LogiPage />
   );
 };
 
